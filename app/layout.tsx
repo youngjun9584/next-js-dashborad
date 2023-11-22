2d9bb886e8c3cb1863215a772a2fa8c0322ee5b0
park <wns9584@gmail.com>
2023-11-22 11:19:36 +0900

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import SideNav from './ui/dashboard/sidenav';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
